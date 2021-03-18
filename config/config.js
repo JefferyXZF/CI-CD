bc344948b2b993b7d9c61ad010bcb23ee3b9f025
xiezhifei <xiezhifei@do1.com.cn>
2021-03-18 20:01:47 +0800

    fixed file

--- config/config.js
+++ config/config.js
@@ -6,29 +6,8 @@ module.exports = {
   // 机器人链接
   baseWebhook: 'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=',
   // 接收人
-  receivers: [ '谢志非', '覃荣邦', '张盛彬', '郑佳娜', '李尚纲' ],
+  receivers: [ ],
   // 人员列表
-  commitPeople: {
-    xiezhifei: {
-      name: '谢志非',
-      phone: '',
-    },
-    lishanggang: {
-      name: '李尚纲',
-      phone: '',
-    },
-    zhangshengbin: {
-      name: '张盛彬',
-      phone: '',
-    },
-    zhengjiana: {
-      name: '郑佳娜',
-      phone: '',
-    },
-    qinrongbang: {
-      name: '覃荣邦',
-      phone: '',
-    },
-  },
+  commitPeople: {},
   mergeTargetBranch: [ 'develop', 'release' ],
 };
